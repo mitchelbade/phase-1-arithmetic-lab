631e05941ce3fe60223a4c6f618cd78365785c33
mitchelbade <mitchelm.bade@gmail.com>
2021-10-06 23:50:37 -0700

    commit tree 0e78bb8ff24aabd5342617fe9592a9e544e7c696

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 16;
+const num4 = 6;
+
+const multiply = num1 * num2;
+
+const random = Math.round(Math.random() * 10) + 1;
+
+const mod = num3 % num4;
+
+const max = Math.max(0, 5, 10, 15, 20);
